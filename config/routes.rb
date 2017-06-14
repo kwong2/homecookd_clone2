ae7172dd89a1141b914d9634a239135063101879
Kevin Wong <k.wong4@yahoo.com>
2017-06-14 16:36:39 -0400

    changed root

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   resources :foods
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'users#new'
+
 end
