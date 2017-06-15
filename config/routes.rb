a82bd074041b7102f9699650f63c4fb36c32764d
Kevin Wong <k.wong4@yahoo.com>
2017-06-15 19:27:58 -0400

    Created Sessions and added redirect route + login method

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   resources :reservations
   resources :foods
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'users#new'
 
+  get '/signup' => 'users#new'
+  post '/signup' =>'users#create'
+  
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
 end
