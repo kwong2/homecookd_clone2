4a9c1981683761d6468e5fb4e9259c3ae90e1028
Alfonso Estremadoyro <alfonsoestre24@gmail.com>
2017-06-15 16:31:06 -0400

    "Added a bunch of shit."

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
   resources :reservations
   resources :foods
   resources :users
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170615194715) do
 
   # These are extensions that must be enabled in order to support this database
@@ -35,6 +34,17 @@ ActiveRecord::Schema.define(version: 20170615194715) do
     t.string "user_id"
   end
 
+  create_table "roles", force: :cascade do |t|
+    t.string "name"
+    t.string "resource_type"
+    t.bigint "resource_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
+    t.index ["name"], name: "index_roles_on_name"
+    t.index ["resource_type", "resource_id"], name: "index_roles_on_resource_type_and_resource_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
@@ -45,13 +55,16 @@ ActiveRecord::Schema.define(version: 20170615194715) do
     t.string "zip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-<<<<<<< HEAD
-    t.boolean "host?"
-    t.boolean "guest?"
-=======
->>>>>>> Hosts
     t.string "username"
     t.string "password_digest"
+  end
+
+  create_table "users_roles", id: false, force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "role_id"
+    t.index ["role_id"], name: "index_users_roles_on_role_id"
+    t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
+    t.index ["user_id"], name: "index_users_roles_on_user_id"
   end
 
 end
