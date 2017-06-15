59cd9c25f18c0038a66b6d3ce15a6364c03ec2fa
Kevin Wong <k.wong4@yahoo.com>
2017-06-15 15:58:01 -0400

    changed columns in foods and reservations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170614211531) do
+ActiveRecord::Schema.define(version: 20170615194715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,18 +20,18 @@ ActiveRecord::Schema.define(version: 20170614211531) do
     t.string "price"
     t.text "description"
     t.string "picture"
-    t.string "host_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_id"
   end
 
   create_table "reservations", force: :cascade do |t|
     t.string "time"
     t.string "location"
     t.string "date"
-    t.string "guest_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -44,8 +44,8 @@ ActiveRecord::Schema.define(version: 20170614211531) do
     t.string "zip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "host?"
-    t.boolean "guest?"
+    t.string "username"
+    t.string "password_digest"
   end
 
 end
