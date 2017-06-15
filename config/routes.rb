bae8f3bbbff4317a39949cd10a6514051eba9fd2
Alfonso Estremadoyro <alfonsoestre24@gmail.com>
2017-06-15 17:39:25 -0400

    Added controllers, routes, and made a added a ruby provide:title method to static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
+  get 'static_pages/about'
 
   resources :reservations
   resources :foods
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#new'
+  root 'static_pages#home'
 
 end
