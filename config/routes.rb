44b88cf1b2d3db6bd8caa680a19fc042d94c9354
Kevin Wong <k.wong4@yahoo.com>
2017-06-17 14:35:08 -0400

    fixed merge conflict in new.html.erb in sessions

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,7 @@ Rails.application.routes.draw do
   
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  post '/logout' => 'sessions#destroy'
   delete '/logout' => 'sessions#destroy'
 end
